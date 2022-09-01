5604c4e69f3171a229853fbe1066796c4c9c65c3
FelisDiligens <47528453+FelisDiligens@users.noreply.github.com>
2022-09-01 12:50:54 +0200

    Adapted the python code to run on Windows + some other changes.

--- joplinRevisionsCleanUp.py
+++ joplinRevisionsCleanUp.py
@@ -1,22 +1,49 @@
-#!/usr/bin/env python3
+# Source:
+# https://github.com/p6ril/joplinRevisionsCleanUp
+#
+# Usage:
+# python.exe revisions-cleanup.py
 
 import sys
 import os
 import os.path
-import glob  # unix style pathname pattern expansion (in particular '~')
+import glob
 import tarfile
 import sqlite3
+import io
+from pathlib import Path
 import subprocess
 
-USER_HOME = os.path.expanduser("~")
-DATABASE_DIR = "/media/veracrypt1/joplin-desktop"  # my Joplin directory is in an encrypted container
-DATABASE_NAME = "database.sqlite"
-JOPLIN_DB = DATABASE_DIR + "/" + DATABASE_NAME
-JOPLIN_DIR = USER_HOME + "/Nextcloud/Joplin"  # local sync directory of Joplin's remote folder
-REVISIONS_ARCHIVE_DIR = ".orphaned_revisions_archive"
-JOPLIN_BACKUP_FILE = USER_HOME + "/JOPLIN_REMOTE_BACKUP.tgz"
-BACKUP = True
-DEBUG = False
+#
+# VARIABLES
+#
+
+USER_HOME             = str(Path.home()) # Windows: %USERPROFILE%, Linux: ~
+ONEDRIVE_DIR          = os.environ['ONEDRIVE'] # Windows has an environment variable: %ONEDRIVE%
+DATABASE_DIR          = os.path.join(USER_HOME, ".config", "joplin-desktop") # Windows: %USERPROFILE%\.config\joplin-desktop, Linux: ~/.config/joplin-desktop
+JOPLIN_DIR            = os.path.join(ONEDRIVE_DIR, "Anwendungen", "Joplin") # local sync directory of Joplin's remote folder
+
+# Backups:
+JOPLIN_BACKUP_FILE    = os.path.join(USER_HOME, "Joplin_Remote_Backup.tgz")
+DO_BACKUP             = False
+
+# Leave as is:
+DATABASE_NAME         = "database.sqlite"
+JOPLIN_DB             = os.path.join(DATABASE_DIR, DATABASE_NAME)
+
+
+#
+# FUNCTIONS
+#
+
+
+def is_joplin_running():
+    """Only works under Windows."""
+    if sys.platform.startswith('win'):
+        return b"Joplin.exe" in subprocess.check_output('tasklist', shell=True)
+    elif sys.platform.startswith('linux'):
+        pass # TODO: Implement for Linux
+    return False
 
 
 def rotation():  # creates a kind of "lifo" rotation, latest rotation is up in the queue
@@ -31,7 +58,7 @@ def rotation():  # creates a kind of "lifo" rotation, latest rotation is up in t
     os.rename(JOPLIN_BACKUP_FILE, JOPLIN_BACKUP_FILE[0:-4] + ".1.tgz")
 
 
-def backupfilter(tarinfo):
+def backup_filter(tarinfo):
     if tarinfo.name.find(REVISIONS_ARCHIVE_DIR) < 0:  # exclude any existing orphaned revision archive
         return tarinfo
     else:
@@ -39,15 +66,14 @@ def backupfilter(tarinfo):
 
 
 def backup():
-    if BACKUP:
-        if os.path.isfile(JOPLIN_BACKUP_FILE):  # a backup file already exists, rotating backups
-            rotation()
-        archive = tarfile.open(JOPLIN_BACKUP_FILE, "w:gz")
-        archive.add(JOPLIN_DIR, filter=backupfilter)
-        archive.close()
+    if os.path.isfile(JOPLIN_BACKUP_FILE):  # a backup file already exists, rotating backups
+        rotation()
+    archive = tarfile.open(JOPLIN_BACKUP_FILE, "w:gz")
+    archive.add(JOPLIN_DIR, filter=backup_filter)
+    archive.close()
 
 
-def getrevisionsfromdb():
+def get_revisions_from_db():
     if os.path.isdir(DATABASE_DIR):
         conn = sqlite3.connect(JOPLIN_DB)
         c = conn.cursor()
@@ -57,27 +83,35 @@ def getrevisionsfromdb():
         conn.close()
         return set(revisions)
     else:
-        print("ERROR: Joplin's Veracrypt container not mounted, aborting !")
+        print("ERROR: Joplin's database not found, aborting !")
         sys.exit(1)
 
 
-def getrevisionsfromfiles():
+def file_filter(fileName):
+    """only keeps Joplin's revision files (type 13)"""
+    result=False
+    f=open(fileName, "r")
+    for l in f:
+        if l.find("type_: 13") != -1:
+            result=True
+            break
+    f.close()
+    return result
+
+
+def file_map(fileName):
+    """extracts the revision ID out of the file name"""
+    i=fileName.rfind(os.path.sep)
+    return fileName[i+1:-3]
+
+
+def get_revisions_from_files():
+    # no grep on Windows!
     if os.path.isdir(JOPLIN_DIR):
-        # searching the web, the consensus seems to be that the standard *nix grep command is so much faster
-        # and more efficient than anything that python can pull off parsing files for matching patterns.
-        # Moving to standard *nix commands here to screen Joplin's revision files (type 13).
-        # This should dramatically improve performances especially when a large number of files is considered.
-        grep = subprocess.run(["cd " + JOPLIN_DIR + ";grep -l 'type_: 13' *.md | cut -d '.' -f1"], capture_output=True,
-                              shell=True, text=True)
-        if grep.returncode == 0:
-            if len(grep.stdout) > 0:
-                revisions = grep.stdout[:-1].split("\n")  # removes the last \n character from the captured output before splitting
-                return set(revisions)
-            else:
-                return set()  # grep returned no matching file i.e. the result is an empty set
-        else:
-            print("ERROR: something went wrong parsing Joplin's files !")
-            sys.exit(1)
+        revisions=glob.glob(JOPLIN_DIR + os.path.sep + "*.md")
+        filtered_list=filter(file_filter, revisions)
+        revisions=map(file_map, filtered_list)
+        return set(revisions)
     else:
         print("ERROR: local sync directory of Joplin's remote not found !")
         sys.exit(1)
@@ -85,11 +119,11 @@ def getrevisionsfromfiles():
 
 def reconcile():
     # check https://www.freecodecamp.org/news/python-sets-detailed-visual-introduction/
-    database = getrevisionsfromdb()
-    files = getrevisionsfromfiles()
+    database = get_revisions_from_db()
+    files = get_revisions_from_files()
     if len(database ^ files) == 0:  # ^ symmetric difference, i.e. elements not in common in both sets
         print("Joplin's database and remote files for revisions are perfectly in sync :)")
-        sys.exit(0)  # everything's good in the world we can stop here
+        return  # everything's good in the world we can stop here
     if not database.issubset(files):
         print("ERROR: some revisions in DB are missing their remote file counterpart !")
         for item in list(database - files):
@@ -97,19 +131,28 @@ def reconcile():
     orphans = list(files - database)
     if len(orphans) > 0:
         print("Number of orphaned revisions found on the remote: " + str(len(orphans)))
-        target = JOPLIN_DIR + "/" + REVISIONS_ARCHIVE_DIR
-        if not os.path.isdir(target):
-            os.mkdir(target)
-        print("Archiving ...")
+        print("Deleting ...")
         for item in orphans:
-            src = JOPLIN_DIR + "/" + item + ".md"
-            dst = target + "/" + item + ".md"
-            if DEBUG:
-                print(src + " => " + dst)
-            else:
-                print("    - " + item + ".md")
-                os.rename(src, dst)
+            path = os.path.join(JOPLIN_DIR, item + ".md")
+            print("    - " + item + ".md")
+            os.remove(path)
 
 
-backup()
-reconcile()
+if __name__ == "__main__":
+    print("""Joplin Revisions Cleanup
+------------------------
+Original by p6ril
+Modified by FelisDiligens
+""")
+    if is_joplin_running():
+        print("ERROR: Please make sure to close Joplin! (File → Quit)")
+        sys.exit(-1)
+
+    if DO_BACKUP:
+        print("Making backup...")
+        backup()
+
+    print("Cleanup...")
+    reconcile()
+    
+    print("Done.")
